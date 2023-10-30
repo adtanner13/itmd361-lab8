b72dbf4105c6386aa98007a3598646a4facbb115
Drew <143019598+adtanner13@users.noreply.github.com>
2023-10-30 12:40:04 -0500

    js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 function init(){
-//add your javascrip between these two lines of code
-var button = document.getElementById('entrybutton');
+  //add your javascrip between these two lines of code
+  var button = document.getElementById('entrybutton');
 
-button.addEventListener('click', showMeText);
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Drew Tanner: "+textbox.value);
+  }
+
+  button.addEventListener('click', showMeText);
 }
 
 window.addEventListener('load', init);
