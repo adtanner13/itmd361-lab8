bedf714582dc618fd13a17a48723b7501bb65271
Drew <143019598+adtanner13@users.noreply.github.com>
2023-10-30 12:38:19 -0500

    initial js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 function init(){
 //add your javascrip between these two lines of code
+var button = document.getElementById('entrybutton');
+
+button.addEventListener('click', showMeText);
+}
 
 window.addEventListener('load', init);
